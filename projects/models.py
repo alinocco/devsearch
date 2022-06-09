ceda90ccfdedb374ba8c2389ca14b272aaeb81e9
Alinocco <alinoco.dia@gmail.com>
2022-06-10 00:56:51 +0600

    L.3.1 - Basics of Models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    def __str__(self):
+        return self.title

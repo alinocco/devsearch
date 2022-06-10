2fff40f5b251b059d51b119798f69704810e669f
Alinocco <alinoco.dia@gmail.com>
2022-06-10 23:27:25 +0600

    L.3.3 - Database Queries

--- projects/views.py
+++ projects/views.py
@@ -1,36 +1,18 @@
+import uuid
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
 
 
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
+
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
+    project = Project.objects.get(uuid=pk)
+    context = {'project': project}
 
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-
-    context = {'project': projectObj}
     return render(request, 'projects/project.html', context)

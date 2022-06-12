4d670a17e0ad8e492825a8e1374498a1887b40bc
Alinocco <alinoco.dia@gmail.com>
2022-06-12 17:39:06 +0600

    L.4.2 - CRUD for Projects

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,5 @@
 import uuid
-
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 
@@ -9,19 +7,48 @@ from .forms import ProjectForm
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
-
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
     project = Project.objects.get(uuid=pk)
     context = {'project': project}
-
     return render(request, 'projects/project.html', context)
 
 
 def createProject(request):
     form = ProjectForm()
-    context = {'form': form}
 
-    return render(request, 'projects/project-form.html', context)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def updateProject(request, pk):
+    project = Project.objects.get(uuid=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid:
+            form.save()
+            return redirect('projects')
+
+    context = {'form': form}
+    return render(request, 'projects/project_form.html', context)
+
+
+def deleteProject(request, pk):
+    project = Project.objects.get(uuid=pk)
+
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+
+    context = {'object': project}
+    return render(request, 'projects/delete_template.html', context)

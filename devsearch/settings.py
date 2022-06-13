2d09f0e8b4354f4f46ab88b439117098a12545f2
Alinocco <alinoco.dia@gmail.com>
2022-06-13 16:15:03 +0600

    L.5.1 - Connect basic static - .css files

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -58,7 +58,11 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
+            # Old way with included os
+            # os.path.join(BASE_DIR, 'templates')
+
+            # New way
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

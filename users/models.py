1107f91fed6c53c2cf7c978973f9495886ecee8a
Alinocco <alinoco.dia@gmail.com>
2022-06-16 02:17:35 +0600

    L.6.2 - Add and Render Profiles

--- users/models.py
+++ users/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+# TODO: Process the null/not-null values
+
 
 class Profile(models.Model):
     """
-    Profile's info is directly connected to User, although User is different. 
+    Profile's info is directly connected to User, although User is different model. 
 
-    We use pre-build User for authentification and profile to share info about the user. 
+    We use pre-build User for authentification and profile to share info among other users. 
     """
     user = models.OneToOneField(
         User, on_delete=models.SET_NULL, null=True, blank=True)
@@ -19,7 +21,7 @@ class Profile(models.Model):
     bio = models.TextField(null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
     profile_image = models.ImageField(
-        upload_to="profiles/", default="", null=True, blank=True)
+        upload_to="users/", default="users/user-default.png", null=True, blank=True)
 
     social_github = models.CharField(max_length=255, null=True, blank=True)
     social_linkedin = models.CharField(max_length=255, null=True, blank=True)
@@ -35,3 +37,25 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    """
+    Skill is connected with Profile by Many-to-One relationship. 
+
+    Defined by every user separately.
+    """
+    owner = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        primary_key=True,
+        editable=False,
+    )
+
+    def __str__(self):
+        return self.name

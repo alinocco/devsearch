e600037d3f7b1eb0912868de5687a956ce689a87
Alinocco <alinoco.dia@gmail.com>
2022-07-06 00:38:28 +0600

    L.13.2 - Connect local DB

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -134,10 +134,24 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'devsearch',
+        'USER': 'postgres',
+        'PASSWORD': 'remont55',
+        'HOST': 'localhost',
+        'PORT': '5432'}
 }
+
+
+# Database SQLite
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
